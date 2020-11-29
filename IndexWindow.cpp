60aa43fa7e0d80a67dda47f6360087596f94c09c
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2020-11-29 23:43:04 +0800

    Update IndexWindow.cpp

--- IndexWindow.cpp
+++ IndexWindow.cpp
@@ -7,7 +7,7 @@ IndexWindow::IndexWindow()
       DataInterface(new DataInterface)
 {
     setWindowTitle(tr("WeatherDataInterface"));
-    // CurrentMenuBar = new QMenuBar(this);
+    CurrentMenuBar = new QMenuBar(this);
     CurrentMenuBar = addMenu(tr("FrontMenu"));
     // CurrentToolBar = new QToolBar(this);
     CurrentToolBar = addToolBar(tr("FrontToolBar"));
